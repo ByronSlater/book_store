1edcfb8a90e0d4b96515b56440addf61b8312678
Byron Slater <byron.slater3@googlemail.com>
2026-08-03 23:06:18 +0100

    workflow test

--- server/__init__.py
+++ server/__init__.py
@@ -14,6 +14,7 @@ def create_app(test_config=None):
         DATABASE='postgresql://postgres:password@book_store_db/book_store',
     )
 
+
     @app.route('/books')
     def books():
         db = get_db()
