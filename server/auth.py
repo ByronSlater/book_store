789f76f431175c944694ba2a85145500f070264d
Byron Slater <byron.slater3@googlemail.com>
2026-08-04 11:13:19 +0100

    auth page

---
+++ server/auth.py
@@ -0,0 +1,18 @@
+import functools
+
+from flask import Blueprint, session, request, url_for, redirect, g, render_template
+
+bp = Blueprint('auth', __name__)
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('auth/register.html', errors=request.args.get('errors', []))
+
+    elif request.method == 'POST':
+        return redirect(url_for('/register', errors=['BIIG PROBLEM']))
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    return 'LOGIN'
