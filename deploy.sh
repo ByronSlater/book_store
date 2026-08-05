07819d0df8155af3b168514ff101d8064431bd88
Byron Slater <byron.slater3@googlemail.com>
2026-08-05 17:20:37 +0100

    trying to fix workflow

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-scp -r -i ~/.ssh/byron_cloud_deployment.pem ./* ec2-user@18.175.50.27:~/book_store
+# scp -r -i ~/.ssh/byron_cloud_deployment.pem ./* ec2-user@18.175.50.27:~/book_store
+
+rsync -azvp --exclude={'node_modules', '*.pyi', '__pycache__'} --delete -e "ssh -i ~/.ssh/byron_cloud_deployment.pem" ./* ec2-user@18.175.50.27:~/book_store
