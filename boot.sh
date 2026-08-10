0552a45ff8ed7e3d8a04d727c6bb77f317379329
Byron Slater <byron.slater3@googlemail.com>
2026-08-10 23:38:15 +0100

    debug mode on

--- boot.sh
+++ boot.sh
@@ -3,4 +3,4 @@ set -e
 export FLASK_DATABASE_URI="postgresql://postgres:password@book_store_db/book_store"
 flask --app server init-db
 flask --app server seed-db
-flask --app server run -h 0.0.0.0 -p 5001
+flask --app server run -h 0.0.0.0 -p 5001 --debug
